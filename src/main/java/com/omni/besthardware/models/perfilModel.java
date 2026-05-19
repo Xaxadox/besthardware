ac0a7780cf52f3487c2dfac10b614f4b560284f5
Caio Rosa <117673906+Xaxadox@users.noreply.github.com>
2026-05-19 12:52:33 -0300

    Refactor perfilModel to remove duplicate nome field

--- src/main/java/com/omni/besthardware/models/perfilModel.java
+++ src/main/java/com/omni/besthardware/models/perfilModel.java
@@ -16,8 +16,12 @@ public class perfilModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @ManyToMany(fetch = FetchType.LAZY)
+    
+    @Column(name = "nome", nullable = false, length = 128)
+    private String nome;
 
+   
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "perfilComponente",
             joinColumns = @JoinColumn(name = "idPerfil"),
             // A chave que aponta para esta classe (Perfil)
@@ -25,15 +29,5 @@ public class perfilModel {
             // A chave que aponta para o outro lado
     )
     //Acesso a tabela perfilComponente, fiz dessa forma para nao quebrar a compatibilidade com o JPA
-    @Column(name = "nome", nullable = false, length = 128)
-    private String nome;
-
-    // Abstração da tabela associativa perfilComponente
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(
-            name = "perfilComponente",
-            joinColumns = @JoinColumn(name = "idPerfil"),
-            inverseJoinColumns = @JoinColumn(name = "idComponente")
-    )
     private List<componenteModel> componentes;
 }
